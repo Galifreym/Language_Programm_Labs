ba878c89462f3648200085a95e0f1d4e2cd3a8e9
Galifreym <81036895+Galifreym@users.noreply.github.com>
2021-05-22 11:36:47 +0300

    Update main.cpp

--- LAB_7_TUSK_2/main.cpp
+++ LAB_7_TUSK_2/main.cpp
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-void findPass(string pass, const string& hash, string name) {
+void findPass(string pass, const string& hash, string name)
+{
     static mutex mtx;
     cout << name+"started\n";
     crypt_data *pCryptData = new crypt_data;
@@ -26,7 +27,7 @@ void findPass(string pass, const string& hash, string name) {
             break;
         }
     } while ( next_permutation( pass.begin(), pass.end() ) );
-        delete pCryptData;
+    delete pCryptData;
     cout << name+"finished\n";
 }
 
@@ -43,14 +44,19 @@ int main ()
     string abc_pass = "123456789";
     thread th_1, th_2,th_3,th_4,th_5,th_6,th_7,th_8;
     if (kolichestvo == 1) {
-        th_1 = thread(findPass, abc_pass, hash3[i], "First ")
-        th_1.join();
+        for (int i = 0; i < kolichestvo; i++) {
+            th_1 = thread(findPass, abc_pass, hash3[i], "First ");
+            th_1.join();
+        }
     } else if (kolichestvo == 2) {
+        for (int i = 0; i < kolichestvo-1; i++) {
             th_1 = thread(findPass, abc_pass, hash3[i], "First ");
             th_2 = thread(findPass, abc_pass, hash3[i+1], "Second ");
             th_1.join();
             th_2.join();
+        }
     } else if(kolichestvo == 4) {
+        for (int i = 0; i < kolichestvo-3; i++) {
             th_1 = thread(findPass, abc_pass, hash3[i], "First ");
             th_2 = thread(findPass, abc_pass, hash3[i+1], "Second ");
             th_3 = thread(findPass, abc_pass, hash3[i+2], "Third ");
@@ -59,7 +65,9 @@ int main ()
             th_2.join();
             th_3.join();
             th_4.join();
+        }
     } else if(kolichestvo == 8) {
+        for (int i = 0; i < kolichestvo-7; i++) {
             th_1 = thread(findPass, abc_pass, hash3[i], "First ");
             th_2 = thread(findPass, abc_pass, hash3[i+1], "Second ");
             th_3 = thread(findPass, abc_pass, hash3[i+2], "Third ");
@@ -76,6 +84,7 @@ int main ()
             th_6.join();
             th_7.join();
             th_8.join();
+        }
     }
     return 0;
 }
